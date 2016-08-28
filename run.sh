f016c944f8ae306e185b39e7f4ddecf10ade7ab2
Heyaw Meteke <heyawmeteke@gmail.com>
2016-08-28 12:47:07 -0700

    fixed git install bug

--- run.sh
+++ run.sh
@@ -6,8 +6,7 @@ function getRepo {
     curl -H "Accept: application/json" -X GET "$base_url/downloadRSA" > /home/ubuntu/.ssh/id_rsa
     eval "$(ssh-agent -s)"
     ssh-add /home/ubuntu/.ssh/id_rsa
-    mkdir /home/ubuntu/service
-    git clone git@github.com:h3y4w/dissect-services.git /home/ubuntu/service
+    git clone git@github.com:h3y4w/dissect-services.git
 }
 sudo mkfs -t ext4 /dev/xvdb
 sudo mkdir /file
