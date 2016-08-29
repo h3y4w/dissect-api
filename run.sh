fc910b03192e750dee987c7c845d9be9401bbbe6
Heyaw Meteke <heyawmeteke@gmail.com>
2016-08-29 00:06:13 -0700

    removed git install from run.sh

--- run.sh
+++ run.sh
@@ -2,11 +2,11 @@
 1.4.6
 
 function getRepo {
-    sudo apt-get install git
     curl -H "Accept: application/json" -X GET "$base_url/downloadRSA" > /home/ubuntu/.ssh/id_rsa
     eval "$(ssh-agent -s)"
     ssh-add /home/ubuntu/.ssh/id_rsa
-    git clone git@github.com:h3y4w/dissect-services.git
+    sudo mkdir /home/ubuntu/dissect-services
+    git clone git@github.com:h3y4w/dissect-services.git /home/ubuntu/dissect-services/ .
 }
 sudo mkfs -t ext4 /dev/xvdb
 sudo mkdir /file
