9d88919a012dd5a8875986f86fdedb57856fb9a6
Heyaw Meteke <heyawmeteke@gmail.com>
2016-08-29 23:02:56 -0700

    added installRepo function to run.sh

--- run.sh
+++ run.sh
@@ -1,14 +1,26 @@
 !/bin/bash
 1.4.6
 
+function installRepo {
+    cd /home/ubuntu/
+    eval "$(ssh-agent -s)"
+    ssh-add id_rsa
+    git clone git@github.com:h3y4w/dissect-workers.get
+}
+
 function runRepo {
     cd /home/ubuntu
     python dissect-workers/test.py
 }
+
+function mountEBS {
 sudo mkfs -t ext4 /dev/xvdb
 sudo mkdir /file
 sudo mount /dev/xvdb /file
+}
 
+mountEBS
+installRepo
 runRepo
 
 
