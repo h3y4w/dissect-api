9c7ed4fdc2136ff522115086cc06a9d6527d48c1
Heyaw Meteke <heyawmeteke@gmail.com>
2016-08-29 23:12:26 -0700

    repalced git clone with simple zip download

--- run.sh
+++ run.sh
@@ -1,16 +1,9 @@
 !/bin/bash
 1.4.6
 
-function installRepo {
-    cd /home/ubuntu/
-    eval "$(ssh-agent -s)"
-    ssh-add id_rsa
-    git clone git@github.com:h3y4w/dissect-workers.get
-}
-
 function runRepo {
     cd /home/ubuntu
-    python dissect-workers/test.py
+    python dissect-workers-master/test.py
 }
 
 function mountEBS {
