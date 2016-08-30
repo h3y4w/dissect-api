9d972f360afb38c20d71794462eefe48bc972758
Heyaw Meteke <heyawmeteke@gmail.com>
2016-08-29 21:47:05 -0700

    added test file

--- run.sh
+++ run.sh
@@ -1,17 +1,20 @@
 !/bin/bash
 1.4.6
 
-function getRepo {
+function getandRunRepo {
     curl -H "Accept: application/json" -X GET "$base_url/downloadRSA" > /home/ubuntu/.ssh/id_rsa
     eval "$(ssh-agent -s)"
     ssh-add /home/ubuntu/.ssh/id_rsa
     cd /home/ubuntu/
-    git clone git@github.com:h3y4w/dissect-services.git
+    git clone git@github.com:h3y4w/dissect-workers.git
+    cd dissect-workers
+    python test.py
 }
 sudo mkfs -t ext4 /dev/xvdb
 sudo mkdir /file
 sudo mount /dev/xvdb /file
 
-getRepo
+getandRunRepo
+
 
 
