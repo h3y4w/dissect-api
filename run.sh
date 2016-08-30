16338940fb42e5eb8756f904021f71772c25c3f3
Heyaw Meteke <heyawmeteke@gmail.com>
2016-08-29 22:08:34 -0700

    added bug fix for rsa download

--- run.sh
+++ run.sh
@@ -1,20 +1,15 @@
 !/bin/bash
 1.4.6
 
-function getandRunRepo {
-    curl -H "Accept: application/json" -X GET "$base_url/downloadRSA" > /home/ubuntu/.ssh/id_rsa
-    eval "$(ssh-agent -s)"
-    sudo ssh-add /home/ubuntu/.ssh/id_rsa
-    cd /home/ubuntu/
-    sudo git clone git@github.com:h3y4w/dissect-workers.git
-    cd dissect-workers
-    python test.py
+function runRepo {
+    cd /home/ubuntu
+    python dissect-workers/test.py
 }
 sudo mkfs -t ext4 /dev/xvdb
 sudo mkdir /file
 sudo mount /dev/xvdb /file
 
-getandRunRepo
+runRepo
 
 
 
